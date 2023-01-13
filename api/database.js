e4362d98d51a9af12753d8b8b454d35767da6c6c
René Valderrey González <rene_valderrey@hotmail.com>
2023-01-13 20:33:34 -0300

    database

--- api/database.js
+++ api/database.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-// const URI = process.env.MONGODB_URI;
-const DB_DEPLOY = process.env.DB_DEPLOY;
+const URI = process.env.MONGODB_URI;
+// const DB_DEPLOY = process.env.DB_DEPLOY;
 
 const connection = () => {
   mongoose.set("strictQuery", false);
   mongoose
-    .connect(DB_DEPLOY, {})
+    .connect(URI, {})
     .then(() => console.log("Database connected!"))
     .catch((err) => console.log(err));
 };
