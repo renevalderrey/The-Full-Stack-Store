28494fb1a30c75b4c09072742add21714a9e0b0c
René Valderrey González <rene_valderrey@hotmail.com>
2023-01-14 02:12:25 -0300

    act

--- client/src/Components/CardFeaturedProducts/CardFeaturedProducts.jsx
+++ client/src/Components/CardFeaturedProducts/CardFeaturedProducts.jsx
@@ -23,9 +23,9 @@ export default function CardFeaturedProducts2({ num }) {
             <img src={oneProduct.image} className={stl.prodImg} />
           </Link>
         </div>
-        <div className={stl.item3}>{oneProduct.name}</div>
+        <div className={stl.item3}>{oneProduct.name.slice(0, 70)}</div>
         <div className={stl.item4}>
-          {oneProduct.description}...
+          {oneProduct.description.slice(0, 105)}...
         </div>
         <div className={stl.item5}>sku: XXX-XXXXXX</div>
         <div className={stl.item6}>${oneProduct.price}</div>
