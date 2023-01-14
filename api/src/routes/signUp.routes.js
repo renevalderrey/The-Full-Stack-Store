dcb63f28b398fa5bfe8dee9fcfaf9ddc248e2fe1
René Valderrey González <rene_valderrey@hotmail.com>
2023-01-14 00:24:06 -0300

    act

--- api/src/routes/signUp.routes.js
+++ api/src/routes/signUp.routes.js
@@ -15,7 +15,7 @@ router.get("/", (req, res) => {
   const { email } = req.body;
   try {
     !email
-    ? res.status(200).json({ message: "Usuario ya existe" })
+    ? res.status(400).json({ message: "Usuario ya existe" })
     : res.status(200).json({ message: "Usuario creado correctamente" });
   } catch (error) {
     res.status(400).json({error: error.message})
