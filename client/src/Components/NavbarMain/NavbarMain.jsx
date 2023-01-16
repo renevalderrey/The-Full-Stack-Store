aac38848b495963aaa95ef5f8f64415c5d35bef2
René Valderrey González <rene_valderrey@hotmail.com>
2023-01-16 09:48:03 -0300

    act

--- client/src/Components/NavbarMain/NavbarMain.jsx
+++ client/src/Components/NavbarMain/NavbarMain.jsx
@@ -2,40 +2,46 @@ import React from "react";
 import Nav from "react-bootstrap/Nav";
 import stl from "./NavbarMain.module.css";
 import Container from "react-bootstrap/Container";
+import { useNavigate } from "react-router-dom";
 
 export default function NavbarHeader() {
+  const navigate = useNavigate();
+
+  const onClickProducts = () =>{
+    navigate("/products")
+  }
   return (
     <>
       <Container fluid="xxl">
         <div className={stl.container}>
           <Nav className="justify-content-center" activeKey="/home">
             <Nav.Item>
-              <Nav.Link className={stl.colors} href="/products">
+              <Nav.Link className={stl.colors} onClick={onClickProducts}>
                 Promociones
               </Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link className={stl.colors} href="/products">
+              <Nav.Link className={stl.colors} onClick={onClickProducts}>
                 Nuevos productos
               </Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link className={stl.colors} href="/products">
+              <Nav.Link className={stl.colors} onClick={onClickProducts}>
                 Hardware
               </Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link className={stl.colors} href="/products">
+              <Nav.Link className={stl.colors} onClick={onClickProducts}>
                 PC Gamers
               </Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link className={stl.colors} href="/products">
+              <Nav.Link className={stl.colors} onClick={onClickProducts}>
                Software
               </Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link className={stl.colors} href="/products">
+              <Nav.Link className={stl.colors} onClick={onClickProducts}>
               Home office
               </Nav.Link>
             </Nav.Item>
